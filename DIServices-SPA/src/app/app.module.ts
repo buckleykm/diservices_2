0de58059d1c3eb9e750c3c752f0c0c437ac315d7
Kevin Buckley <buckleykm@gmail.com>
2018-09-14 10:07:10 -0700

    Wireframe Completed

--- DIServices-SPA/src/app/app.module.ts
+++ DIServices-SPA/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { appRoutes } from './routes';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavComponent,
+      HomeComponent,
+      FooterComponent
+   ],
+   imports: [
+      BrowserModule,
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
